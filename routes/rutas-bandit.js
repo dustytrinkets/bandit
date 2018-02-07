d3cb6d99803dc9fe6ed5ff272260a5e6ef5fd450
Laura <lcorbi@interactionmobile.com>
2018-02-07 14:27:53 +0100

    routes update

--- routes/rutas-bandit.js
+++ routes/rutas-bandit.js
@@ -2,11 +2,13 @@
 
 var express = require ('express');
 var userController = require ('../controllers/userController')
+var bandController = require ('../controllers/bandController')
 
 var api = express.Router();
 
 
 
 api.post('/user', userController.insertUser);
+api.post('/band', bandController.insertBand);
 
 module.exports = api;
